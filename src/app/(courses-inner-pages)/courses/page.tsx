a35b55f1e925035afa936873f1cf774868f8c313
mymonatanisha <tanisha9342@gmail.com>
2025-06-06 12:27:51 +0600

    updated version

--- src/app/(courses-inner-pages)/courses/page.tsx
+++ src/app/(courses-inner-pages)/courses/page.tsx
@@ -2,6 +2,7 @@ import CoursesMain from '@/components/courses-inner-pages/courses/CoursesMain';
 import Wrapper from '@/layout/DefaultWrapper';
 import { Metadata } from 'next';
 import React from 'react';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
     title: "Course Grid Right - Education & Online Courses React NextJs Template",
@@ -12,6 +13,15 @@ const Courses = () => {
         <>
             <Wrapper>
                 <main>
+                    {/* Adsterra Native Banner - Top of Course Filter Page */}
+                    <div id="container-4f198eb91f4b0c0da094b4029c7caad6"></div>
+                    <Script
+                        id="adsterra-native-banner"
+                        strategy="afterInteractive"
+                        src="//pl26850643.profitableratecpm.com/4f198eb91f4b0c0da094b4029c7caad6/invoke.js"
+                        async
+                        data-cfasync="false"
+                    />
                     <CoursesMain />
                 </main>
             </Wrapper>
