b2d85204e517c2d00652ded3b4e759870549cb72
mymonatanisha <tanisha9342@gmail.com>
2025-09-10 20:14:45 +0600

    Clean project structure and fix build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Metadata } from "next";
 import Script from "next/script";
 import React from "react";
 
-
 export const metadata: Metadata = {
   title: "Project-base App Dev Course",
 };
@@ -14,27 +13,20 @@ export const metadata: Metadata = {
 const Home = () => {
   return (
     <>
-        <Wrapper>
-          <main className="main-area">
-            <HomeMain />
-            
-          </main>
-        </Wrapper>
-<<<<<<< HEAD
-        <script type='text/javascript' src='//pl26850584.profitableratecpm.com/8f/88/2b/8f882b070aa288aa986892f6ee6b951d.js'></script>
-    </>
-=======
+      <Wrapper>
+        <main className="main-area">
+          <HomeMain />
+        </main>
+      </Wrapper>
 
-        
-        {/* Adsterra Social Bar script */}
-        <Script
+      {/* Adsterra Social Bar script */}
+      <Script
         id="adsterra-social-bar"
         strategy="afterInteractive"
         src="//pl26850584.profitableratecpm.com/8f/88/2b/8f882b070aa288aa986892f6ee6b951d.js"
       />
-            </>
->>>>>>> 8432736a2bfa2ae6b3cffd8c5790503a6e7a47e3
+    </>
   );
-}
+};
 
-export default Home
+export default Home;
