4122083d9d4b56b8617e027b598bce2c84997b8b
MYMONA AKTER TANISHA <tanisha9342@gmail.com>
2026-02-01 23:34:24 +0600

    Update route.ts

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const res = NextResponse.redirect(new URL("/", request.url));
+  const res = NextResponse.redirect("https://enamnotes.com/");
   res.cookies.set("token", "", { httpOnly: true, path: "/", maxAge: 0 });
   return res;
 }
