3b49263932bbbaddbd3382392054b98417fee1f2
Enamul Huq <126886150+enamulhuq121@users.noreply.github.com>
2026-02-24 10:59:25 +0600

    Update GetRating component to make averageRating optional and add null check

--- src/components/common/GetRating.tsx
+++ src/components/common/GetRating.tsx
@@ -1,34 +1,18 @@
+import React from 'react';
 
-import React from "react";
-interface rattingType {
-    averageRating: number;
+interface GetRatingProps {
+    averageRating?: number;
+    ratings: number[];
 }
-const GetRating = ({ averageRating }: rattingType) => {
-    const getRating = (ratingsNum: number) => {
-        const roundedRatings = Math.floor(ratingsNum); // Round down to the nearest integer
-        const isHalfStar = ratingsNum % 1 !== 0; // Check if there's a decimal part
 
-        const emptyRatingCount = 5 - roundedRatings - (isHalfStar ? 1 : 0);
-        const ratings = [];
+const GetRating: React.FC<GetRatingProps> = ({ averageRating, ratings }) => {
+    const totalRatings = ratings.length;
+    const average = averageRating != null ? averageRating : 0;
 
-        for (let i = 0; i < roundedRatings; i++) {
-            ratings.push(<i className="fa-solid fa-star" key={`l-${i}`}></i>);
-        }
-
-        if (isHalfStar) {
-            ratings.push(<i className="fa-solid fa-star-half" key="fa-star-half"></i>);
-        }
-        for (let i = 0; i < emptyRatingCount; i++) {
-            ratings.push(<i className="fa-solid fa-star" key={`p-${i}`}></i>);
-        }
-
-        return ratings;
-    };
     return (
-        <>
-            {" "}
-            {getRating(averageRating)}{" "}
-        </>
+        <div>
+            <p>Average Rating: {totalRatings > 0 ? average : 'No ratings available'}</p>
+        </div>
     );
 };
 
