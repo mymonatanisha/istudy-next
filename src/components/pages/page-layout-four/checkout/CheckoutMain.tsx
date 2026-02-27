650b7d0821c8e37848024993fad1fb04d3d0f8d2
MYMONA AKTER TANISHA <tanisha9342@gmail.com>
2026-02-27 19:23:32 +0600

    Update payment instructions in CheckoutMain component

--- src/components/pages/page-layout-four/checkout/CheckoutMain.tsx
+++ src/components/pages/page-layout-four/checkout/CheckoutMain.tsx
@@ -276,8 +276,8 @@ const CheckoutMain = () => {
                                     <ul>
                                     <li>Send the course fee using your preferred payment method.</li>
                                       <li>Enter your Transaction ID below.</li>
-                                      <li>Click "Confirm Payment & Enroll".</li>
-                                      <li>Access will be activated after payment verification.</li>
+                                       <li>Click &quot;Confirm Payment & Enroll&quot; button.</li>
+                                      <li>Access will be activated soon.</li>
                                     </ul>
                                 </div>
                                 <div className="checkout-agree">
