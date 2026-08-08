3e8b94f4e055871a635f923238077a835a08f0b8
Enamul Huq <126886150+enamulhuq121@users.noreply.github.com>
2026-08-08 16:47:08 +0600

    feat: update course count for Flutter course

--- src/components/courses-inner-pages/courses/CoursesMain.tsx
+++ src/components/courses-inner-pages/courses/CoursesMain.tsx
@@ -12,7 +12,6 @@ import NiceSelect from '@/components/elements/nice-select/NiceSelect';
 const CoursesMain = () => {
     const [isGridView, setIsGridView] = useState(true);
 
-    //handle toggle
     const handleGridClick = () => {
         setIsGridView(true);
     };
@@ -24,90 +23,47 @@ const CoursesMain = () => {
     return (
         <>
             <Breadcrumbs breadcrumbTitle='Advanced Course Filter' />
-            {/* -- course area start -- */}
             <section className="bd-course-area section-space">
                 <div className="container">
                     <div className="row g-30 align-items-center justify-content-between mb-30">
                         <div className="course-top-meta d-flex-between flex-wrap-small mb-30 gap-30">
                             <div className="bd-top-sorting-left">
-                                <h6 className="bd-sorting-item-found">We found <span>14</span> courses available for you</h6>
+                                <h6 className="bd-sorting-item-found">We found <span>15</span> courses available for you</h6>
                             </div>
                             <div className="bd-top-sorting-right d-flex flex-wrap-small align-items-center gap-15">
                                 <div className="bd-layout-switcher">
-                                    <label className={`bd-filter-type-text bd-grid-filter-text ${isGridView ? "active" : ""}`}>
-                                        Grid
-                                    </label>
-                                    <label className={`bd-filter-type-text bd-list-filter-text ${!isGridView ? "active" : ""}`}>
-                                        List
-                                    </label>
+                                    <label className={`bd-filter-type-text bd-grid-filter-text ${isGridView ? "active" : ""}`}>Grid</label>
+                                    <label className={`bd-filter-type-text bd-list-filter-text ${!isGridView ? "active" : ""}`}>List</label>
                                     <ul className="bd-switcher-btn">
-                                        <li>
-                                            <button
-                                                onClick={handleGridClick}
-                                                className={`bd-filter-layout-trigger bd-grid-filter-trigger ${isGridView ? "active" : ""}`}
-                                            >
-                                                <i className="fa-solid fa-grid"></i>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <button
-                                                onClick={handleListClick}
-                                                className={`bd-filter-layout-trigger bd-list-filter-trigger ${!isGridView ? "active" : ""}`}
-                                            >
-                                                <i className="fa-solid fa-list"></i>
-                                            </button>
-                                        </li>
+                                        <li><button onClick={handleGridClick} className={`bd-filter-layout-trigger bd-grid-filter-trigger ${isGridView ? "active" : ""}`}><i className="fa-solid fa-grid"></i></button></li>
+                                        <li><button onClick={handleListClick} className={`bd-filter-layout-trigger bd-list-filter-trigger ${!isGridView ? "active" : ""}`}><i className="fa-solid fa-list"></i></button></li>
                                     </ul>
                                 </div>
                                 <div className="bd-sorting-select">
-                                    <NiceSelect
-                                        options={courseOrderEnum}
-                                        filterIcon={true}
-                                        defaultCurrent={0}
-                                        onChange={selectHandler}
-                                        name=""
-                                        className="course-orderby"
-                                    />
+                                    <NiceSelect options={courseOrderEnum} filterIcon={true} defaultCurrent={0} onChange={selectHandler} name="" className="course-orderby" />
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="row gy-30">
                         <div className="col-12">
-                            {/* -- course grid style -- */}
                             <div className={`display-layout-grid ${isGridView ? "active" : ""}`} style={{ height: isGridView ? "auto" : "0", overflow: "hidden" }}>
-                                <div className="row g-30">
-                                    <CourseGridCard />
-                                </div>
+                                <div className="row g-30"><CourseGridCard /></div>
                             </div>
-                            {/* -- course grid style end -- */}
-
-                            {/* -- course list style -- */}
                             <div className={`display-layout-list ${!isGridView ? "active" : ""}`} style={{ height: !isGridView ? "auto" : "0", overflow: "hidden" }}>
                                 <div className="bd-course-list with-sidebar">
-                                    {
-                                        coursesData.slice(22, 32).map((item) => (
-                                            item.type === "course" ? (
-                                                <CourseListCard key={item.id} course={item} />
-                                            ) : (
-                                                <AdBoxCard key={item.id} adbox={item} />
-                                            )
-                                        ))
-                                    }
+                                    {coursesData.slice(22, 32).map((item) => (
+                                        item.type === "course" ? <CourseListCard key={item.id} course={item} /> : <AdBoxCard key={item.id} adbox={item} />
+                                    ))}
                                 </div>
                             </div>
-                            {/* -- course list style end -- */}
-                            {/* -- course-more style -- */}
                             <div className="bd-course-more-btn d-flex justify-content-center mt-50">
-                                <Link className="bd-btn btn-outline-border-primary" href="#">Load More <span className="right-icon"><i
-                                    className="fa-duotone fa-spinner"></i></span></Link>
+                                <Link className="bd-btn btn-outline-border-primary" href="#">Load More <span className="right-icon"><i className="fa-duotone fa-spinner"></i></span></Link>
                             </div>
-                            {/* -- course-more style end -- */}
                         </div>
                     </div>
                 </div>
             </section >
-            {/* -- course area end -- */}
         </>
     );
 };
