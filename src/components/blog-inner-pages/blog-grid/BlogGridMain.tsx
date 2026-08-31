5d6fc94700a6df6e7f6d5d448ca5810e9c4a694a
MYMONA AKTER TANISHA <tanisha9342@gmail.com>
2026-08-31 18:22:30 +0600

    Use blog helper getPage to replace magic slice in BlogGridMain

--- src/components/blog-inner-pages/blog-grid/BlogGridMain.tsx
+++ src/components/blog-inner-pages/blog-grid/BlogGridMain.tsx
@@ -1,41 +1,35 @@
-import BlogSidebar from '@/components/common/blog/BlogSidebar';
-import BlogSingleCard from '@/components/common/blog/BlogSingleCard';
-import Breadcrumbs from '@/components/common/Breadcrumb/Breadcrumbs';
-import BasicPagination from '@/components/elements/pagination/BasicPagination';
-import blogData from '@/data/blog-data';
-import React from 'react';
-
-const BlogGridMain = () => {
-    return (
-        <>
-            <Breadcrumbs breadcrumbTitle='Blog Grid' />
-            {/* -- course list area start -- */}
-            <section className="bd-course-list-area section-space">
-                <div className="container">
-                    <div className="row gy-30">
-                        <div className="col-xxl-8 col-xl-8 col-xxl-8 col-lg-8">
-                            <div className="row gy-30">
-                                {
-                                    blogData.slice(22, 30).map((item) => (
-                                        <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6" key={item.id}>
-                                            <BlogSingleCard item={item} />
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                            {/* -- pagination style -- */}
-                            <BasicPagination />
-                            {/* -- pagination style end -- */}
-                        </div>
-                        <div className="col-xxl-4 col-xl-4 col-xxl-4 col-lg-4">
-                            <BlogSidebar />
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* -- course list area end -- */}
-        </>
-    );
-};
-
-export default BlogGridMain;
+*** Begin Patch
+*** Update File: src/components/blog-inner-pages/blog-grid/BlogGridMain.tsx
+@@
+-import BlogSidebar from '@/components/common/blog/BlogSidebar';
+-import BlogSingleCard from '@/components/common/blog/BlogSingleCard';
+-import Breadcrumbs from '@/components/common/Breadcrumb/Breadcrumbs';
+-import BasicPagination from '@/components/elements/pagination/BasicPagination';
+-import blogData from '@/data/blog-data';
+-import React from 'react';
++import BlogSidebar from '@/components/common/blog/BlogSidebar';
++import BlogSingleCard from '@/components/common/blog/BlogSingleCard';
++import Breadcrumbs from '@/components/common/Breadcrumb/Breadcrumbs';
++import BasicPagination from '@/components/elements/pagination/BasicPagination';
++import { getPage } from '@/lib/blog';
++import React from 'react';
+@@
+-    return (
++    const page = 1;
++    const perPage = 8; // replace previous slice(22,30)
++    const { posts } = getPage(page, perPage);
++
++    return (
+@@
+-                                    blogData.slice(22, 30).map((item) => (
+-                                        <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6" key={item.id}>
+-                                            <BlogSingleCard item={item} />
+-                                        </div>
+-                                    ))
++                                    posts.map((item) => (
++                                        <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6" key={item.id}>
++                                            <BlogSingleCard item={item} />
++                                        </div>
++                                    ))
+                                 }
+*** End Patch
